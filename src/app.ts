5f5265116cc1437658b77c7c5eaa5b234ec96448
AJ Cletus <71078838+johnniecletus@users.noreply.github.com>
2026-02-17 15:53:26 +0100

    feat(health): add health check endpoint

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 import { errorHandler } from "@/common/http";
+import { healthRouter } from "@/modules/health/routes";
 
 export const app = express();
 
@@ -10,6 +11,8 @@ app.use(helmet());
 app.use(cors());
 app.use(compression());
 app.use(express.json());
+
+app.use("/api/v1", healthRouter);
 
 
 app.use(errorHandler);
