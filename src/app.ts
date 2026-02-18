07dc4973d066a8b324ef2fe5b4d8387ed7c19eae
AJ Cletus <71078838+johnniecletus@users.noreply.github.com>
2026-02-18 12:35:06 +0100

    refactor(app): enable CORS credentials for cookie-based authentication

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,9 @@ import { walletRouter } from "@/modules/wallet/routes";
 export const app = express();
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    credentials: true
+}));
 app.use(compression());
 app.use(express.json());
 
